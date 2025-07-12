a622dff93a3ee5de78b1b1ce252b2ad3773f2deb
rejoy96 <rejoyjose96@gmail.com>
2025-07-12 11:12:47 +0530

    Edit Api

--- snipbox_backend/snipbox/urls.py
+++ snipbox_backend/snipbox/urls.py
@@ -1,12 +1,13 @@
 
 from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from .views import SnippetCreateView, SnippetDetailView
+from .views import SnippetCreateView, SnippetDetailView, SnippetUpdateView
 
 urlpatterns = [
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('snippets/create/', SnippetCreateView.as_view(), name='snippet-create'),
     path('snippets/<int:pk>/', SnippetDetailView.as_view(), name='snippet-detail'),
-
+    path('snippets/<int:pk>/update/',
+         SnippetUpdateView.as_view(), name='snippet-update'),
 ]
--- snipbox_backend/snipbox/views.py
+++ snipbox_backend/snipbox/views.py
@@ -1,6 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated
-from .models import Snippet
+from .models import Snippet, Tag
 from .serializers import SnippetSerializer
 
 
@@ -15,3 +15,19 @@ class SnippetDetailView(generics.RetrieveAPIView):
 
     def get_queryset(self):
         return Snippet.objects.filter(user=self.request.user)
+
+
+class SnippetUpdateView(generics.UpdateAPIView):
+    serializer_class = SnippetSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Snippet.objects.filter(user=self.request.user)
+
+    def perform_update(self, serializer):
+        tag_title = self.request.data.get("tag")
+        if tag_title:
+            tag, _ = Tag.objects.get_or_create(title=tag_title)
+            serializer.save(tag=tag)
+        else:
+            serializer.save()
